12894a9a344f147c4929b1d2a76561bb74db5c7e
IrynaTich13 <127568026+IrynaTich13@users.noreply.github.com>
2023-11-07 12:21:03 +0300

    Add files via upload
    HW_3

---
+++ HM_3/src/main/java/org/example/StudentGroup.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class StudentGroup {
+    private String groupName;
+    private List<Student> students;
+
+    public StudentGroup(String groupName) {
+        this.groupName = groupName;
+        this.students = new ArrayList<>();
+    }
+
+    public String getGroupName() {
+
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+
+        this.groupName = groupName;
+    }
+
+    public List<Student> getStudents() {
+
+        return students;
+    }
+
+    public void addStudent(Student student) {
+
+        students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+
+        students.remove(student);
+    }
+
+    @Override
+    public String toString() {
+        return "StudentGroup{" +
+                "groupName='" + groupName + '\'' +
+                ", students=" + students +
+                '}';
+    }
+}
---
+++ HM_3/src/main/java/org/example/StudentGroupService.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class StudentGroupService {
+    private List<Stream> streams;
+
+    public StudentGroupService() {
+        streams = new ArrayList<>();
+    }
+
+    public void addStream(Stream stream) {
+        streams.add(stream);
+    }
+
+    public List<Stream> getStreams() {
+        return streams;
+    }
+}
